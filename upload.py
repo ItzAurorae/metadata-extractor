3f5668fb1e0ca3b5877ea0a97d0a3087202f45db
ItzAurorae <156165812+ItzAurorae@users.noreply.github.com>
2025-03-23 02:25:26 -0400

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS  # Import CORS
 from PIL import Image, ExifTags
 import os
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 UPLOAD_FOLDER = 'static/uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'bmp', 'webp'}
